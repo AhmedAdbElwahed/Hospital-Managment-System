c8398afba6643bb21341491b1b22fcf70202a081
Mohamed Abdraboh <mohamedabdraboh015@gmail.com>
2024-05-07 13:01:42 +0300

    Fix appointment scheduling

--- backend-spring/medica/src/main/java/org/hms/medica/appointment/service/AppointmentSchedulingService.java
+++ backend-spring/medica/src/main/java/org/hms/medica/appointment/service/AppointmentSchedulingService.java
@@ -6,6 +6,8 @@ import org.hms.medica.appointment.mapper.PatientAppointmentMapper;
 import org.hms.medica.appointment.model.Appointment;
 import org.hms.medica.appointment.repository.AppointmentRepository;
 import org.hms.medica.doctor.service.DoctorService;
+import org.hms.medica.patient.model.Patient;
+import org.hms.medica.user.service.UserService;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,10 +17,13 @@ public class AppointmentSchedulingService {
   private final AppointmentRepository appointmentRepository;
   private final PatientAppointmentMapper patientAppointmentMapper;
   private final DoctorService doctorService;
+  private final UserService userService;
 
   public Long scheduleAppointment(PatientAppointmentDto patientAppointmentDto) {
     Appointment appointment = patientAppointmentMapper.toEntity(patientAppointmentDto);
     appointment.setDoctor(doctorService.getDoctorById(patientAppointmentDto.getDoctorId()));
+    Patient patient = (Patient) userService.getCurrentUser();
+    appointment.setPatient(patient);
     appointmentValidator.validate(appointment);
     Appointment savedAppointment = appointmentRepository.save(appointment);
     return savedAppointment.getId();
